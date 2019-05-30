b3229f1b32e583db13c8bd25f5e8bb7eb9b96307
Tyler Leung <binglux@gmail.com>
2019-05-31 05:00:32 +0800

    Add tests for main.go

--- main_test.go
+++ main_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHeartBeatHandler(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	if assert.NoError(t, heartBeatHandler(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "pong", rec.Body.String())
+	}
+}
+
